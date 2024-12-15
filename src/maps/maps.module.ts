b58dd0cec81bef2115829ca1915a16303ff9944c
Junior Alves <junioralves.webdeveloper@gmail.com>
2024-12-15 13:43:01 -0300

    Create places

---
+++ src/maps/maps.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { PlacesController } from './places/places.controller';
+import { PlacesService } from './places/places.service';
+
+@Module({
+  controllers: [PlacesController],
+  providers: [
+    PlacesService,
+    {
+      provide: GoogleMapsClient,
+      useValue: new GoogleMapsClient(),
+    },
+  ],
+})
+export class MapsModule {}
---
+++ src/maps/places/places.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+import { PlacesService } from './places.service';
+
+@Controller('places')
+export class PlacesController {
+  constructor(private readonly placesService: PlacesService) {}
+
+  @Get()
+  findPlaces(@Query('text') text: string) {
+    return this.placesService.findPlaces(text);
+  }
+}
